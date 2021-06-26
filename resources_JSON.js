aada2f53a78d0fc7173b4130739c9ae61325737a
aleitner <leitnersalex@gmail.com>
2021-06-26 15:48:22 -0400

    Begin populating table data

--- resources_JSON.js
+++ resources_JSON.js
@@ -8,5 +8,41 @@ var resources_json = [
         "free": {},
         "purchase": {"amazon": "https://www.amazon.com/dp/1603293116"},
         "url" : ""
-    }
+    },
+    {
+        "title" : "An Introduction to Old English",
+        "author": "Hogg",
+        "date": "2012",
+        "type" : "Textbook",
+        "free": {"free url": "https://gustavorubinoernesto.com/wp-content/uploads/2021/11/Richard-Hogg-An-Introduction-to-Old-English-Edinburgh-University.pdf"},
+        "purchase": {},
+        "url" : ""
+    },
+    {
+        "title" : "Complete Old English",
+        "author": "Mark Atherton",
+        "date": "2019",
+        "type" : "Textbook",
+        "free": {},
+        "purchase": {"amazon": "https://www.amazon.com/dp/1473627923"},
+        "url" : ""
+    },
+    {
+        "title" : "A Guide to Old English, 8th Edition",
+        "author": "Bruce Mitchell and Fred C. Robinson",
+        "date": "2011",
+        "type" : "Textbook",
+        "free": {},
+        "purchase": {"amazon": "https://www.amazon.com/dp/0470671076"},
+        "url" : ""
+    },
+    {
+        "title" : "Learn Old English with Leofwinn",
+        "author": "Matt Love",
+        "date": "2013",
+        "type" : "Textbook",
+        "free": {},
+        "purchase": {"amazon": "https://www.amazon.com/dp/189828167X"},
+        "url" : ""
+    },
 ]
