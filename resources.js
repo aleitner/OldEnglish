7bc7b6e5eecf3b14cd50f946e48921d1e1ad5215
aleitner <leitnersalex@gmail.com>
2021-06-26 16:02:03 -0400

    Simplify urls, turn authors into array

--- resources.js
+++ resources.js
@@ -20,30 +20,33 @@ function loadResources() {
 
         var row = table.insertRow(i);
 
-        var title = row.insertCell(0);
-        var author = row.insertCell(1);
-        var url = row.insertCell(2);
-        var date = row.insertCell(3);
-        var type = row.insertCell(4);
+        var titleCell = row.insertCell(0);
+        var authorsCell = row.insertCell(1);
+        var hyperlinksCell = row.insertCell(2);
+        var dateCell = row.insertCell(3);
+        var typeCell = row.insertCell(4);
 
-        title.innerHTML = resource.title;
-        author.innerHTML = resource.author;
-        date.innerHTML = resource.date;
-        type.innerHTML = resource.type;
+        titleCell.innerHTML = resource.title;
+        dateCell.innerHTML = resource.date;
+        typeCell.innerHTML = resource.type;
+        
+        // Set Authors
+        var authors = ""
+        for(var k = 0; k < resource.authors.length; k++) {
+            authors = `${authors} ${resource.authors[k]},`
+        }
 
+        // Remove ending comma
+        authors = authors.slice(0, -1)
+
+        authorsCell.innerHTML = authors;
+
+        // Set Hyperlinks
         var urls = ""
-        for (const [key, value] of Object.entries(resource.free)) {
+        for (const [key, value] of Object.entries(resource.hyperlinks)) {
             urls = `${urls} <a href="${value}">${key}</a>` 
         }
 
-        for (const [key, value] of Object.entries(resource.purchase)) {
-            urls = `${urls} <a href="${value}">${key}</a>` 
-        }
-
-        if (!isEmpty(resource.url)) {
-            urls = `${urls} <a href="${resource.url}">Link</a>` 
-        }
-
-        url.innerHTML = urls 
+        hyperlinksCell.innerHTML = urls 
     }
 }
--- resources_JSON.js
+++ resources_JSON.js
@@ -2,47 +2,37 @@
 var resources_json = [ 
     {
         "title" : "An Introduction to Old English",
-        "author": "Jonathan Evans",
+        "authors": ["Jonathan Evans"],
         "date": "2021",
         "type" : "Textbook",
-        "free": {},
-        "purchase": {"amazon": "https://www.amazon.com/dp/1603293116"},
-        "url" : ""
+        "hyperlinks": {"amazon": "https://www.amazon.com/dp/1603293116"},
     },
     {
         "title" : "An Introduction to Old English",
-        "author": "Hogg",
+        "authors": ["Hogg"],
         "date": "2012",
         "type" : "Textbook",
-        "free": {"free url": "https://gustavorubinoernesto.com/wp-content/uploads/2021/11/Richard-Hogg-An-Introduction-to-Old-English-Edinburgh-University.pdf"},
-        "purchase": {},
-        "url" : ""
+        "hyperlinks": {"free url": "https://gustavorubinoernesto.com/wp-content/uploads/2021/11/Richard-Hogg-An-Introduction-to-Old-English-Edinburgh-University.pdf"},
     },
     {
         "title" : "Complete Old English",
-        "author": "Mark Atherton",
+        "authors": ["Mark Atherton"],
         "date": "2019",
         "type" : "Textbook",
-        "free": {},
-        "purchase": {"amazon": "https://www.amazon.com/dp/1473627923"},
-        "url" : ""
+        "hyperlinks": {"amazon": "https://www.amazon.com/dp/1473627923"},
     },
     {
         "title" : "A Guide to Old English, 8th Edition",
-        "author": "Bruce Mitchell and Fred C. Robinson",
+        "authors": ["Bruce Mitchell", "Fred C. Robinson"],
         "date": "2011",
         "type" : "Textbook",
-        "free": {},
-        "purchase": {"amazon": "https://www.amazon.com/dp/0470671076"},
-        "url" : ""
+        "hyperlinks": {"amazon": "https://www.amazon.com/dp/0470671076"},
     },
     {
         "title" : "Learn Old English with Leofwinn",
-        "author": "Matt Love",
+        "authors": ["Matt Love"],
         "date": "2013",
         "type" : "Textbook",
-        "free": {},
-        "purchase": {"amazon": "https://www.amazon.com/dp/189828167X"},
-        "url" : ""
-    },
+        "hyperlinks": {"amazon": "https://www.amazon.com/dp/189828167X"},
+    }
 ]
