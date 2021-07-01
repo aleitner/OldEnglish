de0d52088894a5bf093bd9c248834f797e9caf93
aleitner1 <aleitner@maxex.com>
2021-07-01 12:07:28 -0400

    Filter github issues

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,7 +1,11 @@
+var newsPosts = []
+var labels = ['news']
+var users = ['aleitner']
+
 function loadBlogposts() {
     var resp = httpGet("https://api.github.com/repos/aleitner/OldEnglish/issues")
     respAsJSON = JSON.parse(resp)
-    console.log(respAsJSON)
+    newsPosts = filterPosts(respAsJSON)
 }
 
 function httpGet(theUrl) {
@@ -10,4 +14,35 @@ function httpGet(theUrl) {
     xmlHttp.setRequestHeader('Content-type', 'application/vnd.github.v3+json');
     xmlHttp.send(null);
     return xmlHttp.responseText;
+}
+
+function filterPosts(posts) {
+    var filteredPosts = []
+
+    for (var i = 0; i < posts.length; i++) {
+        if (doesPostHaveValidLabel(posts[i]) && doesPostHaveValidUser(posts[i])) {
+            filteredPosts.push(posts[i])
+        }
+    }
+
+    return filteredPosts
+}
+
+function doesPostHaveValidLabel(post) {
+    for (var i = 0; i < post.labels.length; i++) {
+        console.log()
+        if (labels.indexOf(post.labels[i].name) != -1) {
+            return true
+        }
+    }
+
+    return false
+}
+
+function doesPostHaveValidUser(post) {
+    if (users.indexOf(post.user.login) != -1) {
+        return true
+    }
+
+    return false
 }
