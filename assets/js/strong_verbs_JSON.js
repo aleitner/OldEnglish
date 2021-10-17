4f280526de7f81bda199624bffcec7f7bd0506a5
aleitner <leitnersalex@gmail.com>
2021-10-17 00:25:48 -0400

    Added class 2 strong verbs

--- assets/js/strong_verbs_JSON.js
+++ assets/js/strong_verbs_JSON.js
@@ -13,7 +13,7 @@ var strong_verbs_json = [
         preteritePl: "fundon",
         pastParticiple: "funden",
         meaning: "to find",
-        verbClass: "2"
+        verbClass: "3"
     },
     {
         infinitive: "drīfan",
@@ -76,7 +76,7 @@ var strong_verbs_json = [
         preteriteSg: "sċrāþ",
         preteritePl: "sċriðon",
         pastParticiple: "sċriðen",
-        meaning: "to glide, to creep",
+        meaning: "to glide, creep",
         verbClass: "1"
     },
     {
@@ -92,7 +92,7 @@ var strong_verbs_json = [
         preteriteSg: "wāt",
         preteritePl: "witon",
         pastParticiple: "witen",
-        meaning: "to go, to depart",
+        meaning: "to go, depart",
         verbClass: "1"
     },
     {
@@ -111,4 +111,156 @@ var strong_verbs_json = [
         meaning: "to cover",
         verbClass: "1"
     },
+    {
+        infinitive: "clēofan",
+        preteriteSg: "clēaf",
+        preteritePl: "clufon",
+        pastParticiple: "clofen",
+        meaning: "to cut, cleave",
+        verbClass: "2"
+    },
+    {
+        infinitive: "nēotan",
+        preteriteSg: "nēat",
+        preteritePl: "nuton",
+        pastParticiple: "noten",
+        meaning: "to use, enjoy, employ",
+        verbClass: "2"
+    },
+    {
+        infinitive: "ċēosan",
+        preteriteSg: "ċēas",
+        preteritePl: "curon",
+        pastParticiple: "coren",
+        meaning: "to choose",
+        verbClass: "2"
+    },
+    {
+        infinitive: "frēosan",
+        preteriteSg: "frēas",
+        preteritePl: "fruron",
+        pastParticiple: "froren",
+        meaning: "to freeze",
+        verbClass: "2"
+    },
+    {
+        infinitive: "bēodan",
+        preteriteSg: "bēad",
+        preteritePl: "budon",
+        pastParticiple: "boden",
+        meaning: "to command, decree, summon",
+        verbClass: "2"
+    },
+    {
+        infinitive: "drēogan",
+        preteriteSg: "drēag",
+        preteritePl: "drugon",
+        pastParticiple: "drogen",
+        meaning: "to experience, suffer, endure",
+        verbClass: "2"
+    },
+    {
+        infinitive: "grēotan",
+        preteriteSg: "grēat",
+        preteritePl: "gruton",
+        pastParticiple: "groten",
+        meaning: "to cry, lament",
+        verbClass: "2"
+    },
+    {
+        infinitive: "sċēotan",
+        preteriteSg: "sċēat",
+        preteritePl: "sċuton",
+        pastParticiple: "sċoten",
+        meaning: "to shoot, thrust, hurl",
+        verbClass: "2"
+    },
+    {
+        infinitive: "brūcan",
+        preteriteSg: "brēac",
+        preteritePl: "brucon",
+        pastParticiple: "brocen",
+        meaning: "to enjoy, use",
+        verbClass: "2"
+    },
+    {
+        infinitive: "lūcan",
+        preteriteSg: "lēac",
+        preteritePl: "lucon",
+        pastParticiple: "locen",
+        meaning: "to close, lock",
+        verbClass: "2"
+    },
+    {
+        infinitive: "crūdan",
+        preteriteSg: "crēad",
+        preteritePl: "crudon",
+        pastParticiple: "croden",
+        meaning: "to press, drive, hasten",
+        verbClass: "2"
+    },
+    {
+        infinitive: "būgan",
+        preteriteSg: "bēag",
+        preteritePl: "bugon",
+        pastParticiple: "bogen",
+        meaning: "to bend",
+        verbClass: "2"
+    },
+    {
+        infinitive: "onlūtan",
+        preteriteSg: "onlēat",
+        preteritePl: "onluton",
+        pastParticiple: "onloten",
+        meaning: "to incline, bow down",
+        verbClass: "2"
+    },
+    {
+        infinitive: "sūcan",
+        preteriteSg: "sēac",
+        preteritePl: "sucon",
+        pastParticiple: "socen",
+        meaning: "to suck",
+        verbClass: "2"
+    },
+    {
+        infinitive: "sċūfan",
+        preteriteSg: "sċēaf",
+        preteritePl: "sċufon",
+        pastParticiple: "sċofen",
+        meaning: "to shove, push",
+        verbClass: "2"
+    },
+    {
+        infinitive: "slūpan",
+        preteriteSg: "slēap",
+        preteritePl: "slupon",
+        pastParticiple: "slopen",
+        meaning: "to slip",
+        verbClass: "2"
+    },
+    {
+        infinitive: "smūgan",
+        preteriteSg: "smēag",
+        preteritePl: "smugon",
+        pastParticiple: "smogen",
+        meaning: "to creep, crawl",
+        verbClass: "2"
+    },
+    {
+        infinitive: "tēon",
+        preteriteSg: "tēah",
+        preteritePl: "tugon",
+        pastParticiple: "togen",
+        meaning: "to draw",
+        verbClass: "2"
+    },
+    {
+        infinitive: "flēon",
+        preteriteSg: "flēah",
+        preteritePl: "flugon",
+        pastParticiple: "flogen",
+        meaning: "to fly, flee",
+        verbClass: "2"
+    }
 ]
